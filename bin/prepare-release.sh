bc17d1f376538bc2ddc49caa6ce1ce45654e5cdb
Marcus Stade <marcus@stade.se>
2015-12-02 17:41:13 +0100

    Make sure to add dependencies to path

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+pushd ${DIR} > /dev/null
+PATH=$(npm bin):${PATH}
+popd > /dev/null
+
 usage() {
 cat <<USAGE
 Usage: prepare-release <level> [-m|--message <message>]
